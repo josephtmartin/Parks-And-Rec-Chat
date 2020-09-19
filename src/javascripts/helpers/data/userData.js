0e6b05df46a2bb038cbe8c945f4fa614b1aa067e
josephtmartin <goldglovejo13@gmail.com>
2020-09-18 20:07:55 -0500

    trying to fix images on deploy

--- src/javascripts/helpers/data/userData.js
+++ src/javascripts/helpers/data/userData.js
@@ -1,12 +1,21 @@
+import Leslie from '../../../assets/images/leslie.png';
+import Ron from '../../../assets/images/ron.png';
+import Garry from '../../../assets/images/garry.png';
+import April from '../../../assets/images/april.png';
+import Jean from '../../../assets/images/jean-ralphio.png';
+import Tom from '../../../assets/images/tom.png';
+import Donna from '../../../assets/images/donna.png';
+import Pawnee from '../../../assets/images/pawnee.png';
+
 const users = [
-  { id: 'user1', name: 'Leslie Knope', img: '../images/leslie.png' },
-  { id: 'user2', name: 'Ron Swanson', img: '../images/ron.png' },
-  { id: 'user3', name: 'Garry Gergich', img: '../images/garry.png' },
-  { id: 'user4', name: 'April Ludgate', img: '../images/april.png' },
-  { id: 'user5', name: 'Jean Ralphio', img: '../images/jean-ralphio.png' },
-  { id: 'user6', name: 'Tom Haverford', img: '../images/tom.png' },
-  { id: 'user7', name: 'Donna Meagle', img: '../images/donna.png' },
-  { id: 'user8', name: 'Pawnee Citizen', img: '../images/pawnee.png' },
+  { id: 'user1', name: 'Leslie Knope', img: `${Leslie}` },
+  { id: 'user2', name: 'Ron Swanson', img: `${Ron}` },
+  { id: 'user3', name: 'Garry Gergich', img: `${Garry}` },
+  { id: 'user4', name: 'April Ludgate', img: `${April}` },
+  { id: 'user5', name: 'Jean Ralphio', img: `${Jean}` },
+  { id: 'user6', name: 'Tom Haverford', img: `${Tom}` },
+  { id: 'user7', name: 'Donna Meagle', img: `${Donna}` },
+  { id: 'user8', name: 'Pawnee Citizen', img: `${Pawnee}` },
 ];
 
 const getUsers = () => users;
